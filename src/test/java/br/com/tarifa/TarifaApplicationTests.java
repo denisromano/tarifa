6b9574d7516fab255d970fd2ad1e2d9ea20eedc8
unknown <denis.romano@gmail.com>
2019-12-15 18:52:40 -0300

    commit inicial

---
+++ src/test/java/br/com/tarifa/TarifaApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.tarifa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TarifaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
